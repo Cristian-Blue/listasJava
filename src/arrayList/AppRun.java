823cdb7e65e1ad704d8b5565f66e62119de49770
Cristian Afanador <crsitanblue@gmail.com>
2026-09-21 21:21:29 -0500

    fix: example arrayList

--- src/arrayList/AppRun.java
+++ src/arrayList/AppRun.java
@@ -1,16 +1,35 @@
 package arrayList;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.ArrayList; 
+public class AppRun {
 
-public class AppRun{
-    
-    public void run(){
+    public void run() {
         ArrayList<Integer> arreglo = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        int a;
-        System.out.println("Ingrese numero");
-        a = sc.nextInt();
+        arreglo.add(1);
+        arreglo.add(2);
+        arreglo.add(3);
+
+        arreglo.remove(1);
+        for (int i = 0; i < arreglo.size(); i++) {
+            System.out.println(arreglo.get(i) + " i:" + i);
+        }
+        arreglo.add(1, 2);
+        arreglo.forEach(res -> {
+            System.out.println(res);
+        });
+        System.out.println("...............");
+        System.out.println(
+                arreglo.contains(2));
+        System.out.println(
+                arreglo.contains(5));
+        for (Integer num : arreglo) {
+            System.out.println(num);
+        }
+        arreglo.clear();
+        System.out.println("Clear");
+        for (Integer num : arreglo) {
+            System.out.println(num);
+        }
 
     }
 }
